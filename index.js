8be22ec7094bc05689feb27fe2c58168c51e5a49
agus-mb <munosagustina3@gmail.com>
2023-09-18 23:13:50 -0300

    js de menu desplegable listo

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 
+const navbarImages = document.querySelectorAll('.navbar-img');
+
+navbarImages.forEach((img) => {
+  img.addEventListener('click', () => {
+    const navbarItem = img.nextElementSibling; 
+    navbarItem.classList.toggle('navbar-item-show');
+  });
+
+
+
+});
 
 
 //crear una function que elimine el texto de la li y tambien redondee la img, le agrega x class a uno y Y class a otro
+
+//a lo anterior mencinado: que de base este chiquito y cuando se activa se agranda, o sea ir al css y hacerlo chiquito primero que la modificacion sea una class agrandadora.
