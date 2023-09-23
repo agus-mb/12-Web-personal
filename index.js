913f6eec7cfe22101f83ffa46fe83bf767e1e70a
agus-mb <munosagustina3@gmail.com>
2023-09-22 21:51:17 -0300

    funcionalidad de menu

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 
 const navbarImages = document.querySelectorAll('.navbar-img');
+const navbarList= document.querySelectorAll('.navbar-list');
+const elementosAModificar = document.querySelectorAll('.navbar-li');
+
+
 
 navbarImages.forEach((img) => {
+
   img.addEventListener('click', () => {
     const navbarItem = img.nextElementSibling; 
     navbarItem.classList.toggle('navbar-item-show');
+
+    if (window.innerWidth <= 992) {
+        elementosAModificar.forEach((elemento) => {
+           /* elemento.classList.add('ampliado')*/
+           elemento.classList.toggle('ampliado');
+
+        })
+
+    }
+
+
   });
 
 
-
+ 
 });
+
 
 
 //crear una function que elimine el texto de la li y tambien redondee la img, le agrega x class a uno y Y class a otro
